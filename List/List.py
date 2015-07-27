e4c614b2f2ad612f6eb6cf1ea4362db9c69c6ba7
SwanCode <fantasydreamer05@gmail.com>
2015-07-27 15:44:12 +0800

    List

--- List/List.py
+++ List/List.py
@@ -3,6 +3,7 @@ names = ['Sam', 'Kim', 'Jimmy', 'Lynda']
 
 names.append("Alexandra")
 
+del names[-1] #another method of remove beside names.remove(0)
 names.sort()
 for guest in names :
    print(guest)
